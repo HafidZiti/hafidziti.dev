89d1268f0db0c256a4c4e833d78fb19e9fb3a7ee
Hafid <hafid.ziti@viseo.com>
2022-06-02 18:29:18 +0200

    add skills file

---
+++ data/skills.ts
@@ -0,0 +1,90 @@
+import {
+  SiReact,
+  SiAngular,
+  SiNextdotjs,
+  SiJavascript,
+  SiTypescript,
+  SiApollographql,
+  SiSass,
+  SiCss3,
+  SiGit,
+  SiHtml5,
+  SiJest,
+  SiTestinglibrary,
+  SiChakraui,
+  SiJava,
+  SiSpringboot,
+  SiStyledcomponents,
+  SiRedux,
+  SiNodedotjs,
+  SiExpress,
+  SiVercel,
+  SiFirebase,
+  SiGithub,
+  SiGitlab,
+  SiNestjs,
+  // SiPrettier,
+  // SiEslint,
+  //Shopify
+  //Webpack
+  // framer
+  //Yarn, NPM,
+} from "react-icons/si";
+
+export const mainSkills = [
+  {
+    name: "Javascript ES6",
+    level: 90,
+  },
+  {
+    name: "React.js",
+    level: 90,
+  },
+  {
+    name: "Angular2+",
+    level: 80,
+  },
+  {
+    name: "Next.js",
+    level: 85,
+  },
+  {
+    name: "Node.js",
+    level: 70,
+  },
+  {
+    name: "Communication",
+    level: 90,
+  },
+  {
+    name: "Typescript",
+    level: 80,
+  },
+];
+
+export const skills = [
+  { name: "React.js", Icon: SiReact },
+  { name: "Angular", Icon: SiAngular },
+  { name: "Next.js", Icon: SiNextdotjs },
+  { name: "Redux", Icon: SiRedux },
+  { name: "Apollo GraphQL", Icon: SiApollographql },
+  { name: "Javascript", Icon: SiJavascript },
+  { name: "Typescript", Icon: SiTypescript },
+  { name: "HTML5", Icon: SiHtml5 },
+  { name: "CSS3", Icon: SiCss3 },
+  { name: "Sass", Icon: SiSass },
+  { name: "Styled-components", Icon: SiStyledcomponents },
+  { name: "Chakra UI", Icon: SiChakraui },
+  { name: "Jest", Icon: SiJest },
+  { name: "Testing Library", Icon: SiTestinglibrary },
+  { name: "Node.js", Icon: SiNodedotjs },
+  { name: "Git", Icon: SiGit },
+  { name: "GitHub", Icon: SiGithub },
+  { name: "GitLab", Icon: SiGitlab },
+  { name: "Vercel", Icon: SiVercel },
+  { name: "Firebase", Icon: SiFirebase },
+  { name: "Java", Icon: SiJava, dabbled: true },
+  { name: "Express.js", Icon: SiExpress, dabbled: true },
+  { name: "Spring Boot", Icon: SiSpringboot, dabbled: true },
+  { name: "NestJS", Icon: SiNestjs, dabbled: true },
+];
