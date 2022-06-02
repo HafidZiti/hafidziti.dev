955d15671067ed821bd1c25fc20ac6a158ddb79b
Hafid <hafid.ziti@viseo.com>
2022-06-02 18:33:57 +0200

    add component to display social icons

---
+++ src/components/Socials/index.tsx
@@ -0,0 +1,32 @@
+import { Image, Link } from "@chakra-ui/react";
+import { motion } from "framer-motion";
+import { Social } from "../../types/social";
+
+type socialProps = {
+  socials: Social[];
+};
+
+export const Socials: React.FC<socialProps> = (props: socialProps) => {
+  return (
+    <>
+      {props.socials.map((social: Social, index: number) => {
+        return (
+          <motion.div
+            whileHover={{
+              scale: 1.25,
+            }}
+            whileTap={{ scale: 0.9 }}
+            key={index}
+          >
+            <Link href={social.link} isExternal>
+              <Image
+                src={`${social.name}.svg`}
+                alt={`${social.name}.logo`}
+              ></Image>
+            </Link>
+          </motion.div>
+        );
+      })}
+    </>
+  );
+};
