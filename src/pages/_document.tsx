68921d9baf748c4110c8c0b08ef7931c2d2a0f9f
HafidZiti <ziti.hafid@gmail.com>
2022-06-16 10:47:44 +0200

    add favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,6 +11,12 @@ export default class Document extends NextDocument {
             name="description"
             content="I'm Hafid Ziti, a self-taught front-end develop, interested in the web, JS lover"
           />
+          <link
+            rel="icon"
+            sizes="any"
+            type="image/svg+xml"
+            href="/favicon.svg"
+          />
         </Head>
         <body>
           {/* Make Color mode to persists when you refresh the page. */}
